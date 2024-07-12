4290d88b8312cc763af7968876b03425f0f354a3
Alessandro Palumbo <163033493+AlessandroPalumboDev@users.noreply.github.com>
2024-07-12 19:50:23 +0200

    [UPD] Gestione CRUD per view create

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -3,7 +3,13 @@
 @section('main')
 
     <div class="container">
+        <div class="row my-4 justify-content-center">
+            <div class="col-3">
+                <a href="{{route('comics.create')}}" class="btn btn-dark">Aggiungi Fumetto</a>
+            </div>
+        </div>
         <div class="row align-items-center jsutify-content-cener">
+
             @foreach ($comics as $comic)
                 
                 <div class="col-3 mt-4">
--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -9,8 +9,11 @@
                 <div class="card">
                     <img src="{{ $comic->thumb}}" class="card-img-top" alt="{{$comic->title}}">
                     <div class="card-body">
-                    <h5 class="card-title">{{$comic->title}}</h5>
-                    <p class="card-text">{{$comic->type}}</p>
+                    <h4 class="card-title">{{$comic->title}}</h4>
+                    <p class="card-text">Type: {{$comic->type}}</p>
+                    <p class="card-text">Series: {{$comic->series}}</p>
+                    <p class="card-text">Sale date: {{$comic->sale_date}}</p>
+                    <p class="card-text">Price: {{$comic->price}} $</p>
                     <p class="card-text">{{$comic->description}}</p>
                     <a href="{{route('comics.index')}}" class="btn btn-dark">Indietro</a>
                     </div>
