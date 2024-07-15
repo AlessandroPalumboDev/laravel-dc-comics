93a5a3fd2eec0807648e621b47219b7aca740a74
Alessandro Palumbo <163033493+AlessandroPalumboDev@users.noreply.github.com>
2024-07-15 16:52:13 +0200

    [FEAT] Cambio gestione CRUD per view store senza fare il mapping a mano

--- app/Http/Controllers/Admin/ComicController.php
+++ app/Http/Controllers/Admin/ComicController.php
@@ -35,15 +35,15 @@ class ComicController extends Controller
 
         $comic = new Comic();
 
-        $comic->title = $data['title'];
-        $comic->description = $data['description'];
-        $comic->thumb = $data['thumb'];
-        $comic->price = $data['price'];
-        $comic->series = $data['series'];
-        $comic->sale_date = $data['sale_date'];
-        $comic->type = $data['type'];
+        // $comic->title = $data['title'];
+        // $comic->description = $data['description'];
+        // $comic->thumb = $data['thumb'];
+        // $comic->price = $data['price'];
+        // $comic->series = $data['series'];
+        // $comic->sale_date = $data['sale_date'];
+        // $comic->type = $data['type'];
 
-
+        $comic->fill($data); // ha bisogno di save anche se non faccio il mapping a mano
         $comic->save();
 
         return redirect()->route('comics.show', $comic->id);
@@ -79,7 +79,7 @@ class ComicController extends Controller
     public function update(Request $request, Comic $comic)
     {
         $data = $request->all();
-        $comic->update($data);
+        $comic->update($data); // non ha bisogno di save anche se non faccio il mapping a mano
 
         return redirect()->route('comics.show', $comic->id);
 
